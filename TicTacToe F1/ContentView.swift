59ed6271ed54639419a51f96947b245ed1fff257
Joaovictordsl <joaovictordsl00@gmail.com>
2024-01-01 22:54:37 -0300

    Initial Commit

---
+++ TicTacToe F1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe F1
+//
+//  Created by Pedro on 01/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe F1/TicTacToe_F1App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_F1App.swift
+//  TicTacToe F1
+//
+//  Created by Pedro on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_F1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
