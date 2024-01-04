b5d9c097a77d0a44d2542d365251f3c1bad6c698
Joaovictordsl <joaovictordsl00@gmail.com>
2024-01-04 00:28:49 -0300

    trying stuff

--- TicTacToe F1/ContentView.swift
+++ TicTacToe F1/ContentView.swift
@@ -12,13 +12,32 @@ struct ContentView: View {
     
     var body: some View {
         
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+            .ignoresSafeArea(.all)
         
-        
-        VStack {
+            VStack{
+                Text("Belo Horizonte, MG")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding()
+                VStack{
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("27°")
+                        
+                }
+                Spacer()
+            }
             
-            Text("Olá")
         }
-        .padding()
+        
     }
 }
 
