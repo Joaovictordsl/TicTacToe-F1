3c431d6428b2f1fe9f46cd4c8c208d85b6a3fd75
Joaovictordsl <joaovictordsl00@gmail.com>
2024-01-18 01:19:35 -0300

    Trying shapes

--- TicTacToe F1/ContentView.swift
+++ TicTacToe F1/ContentView.swift
@@ -12,33 +12,50 @@ struct ContentView: View {
     
     var body: some View {
         
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
-                           startPoint: .top,
-                           endPoint: .bottom)
-            .ignoresSafeArea(.all)
-        
-            VStack{
-                Text("Belo Horizonte, MG")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundStyle(.white)
-                    .padding()
-                VStack{
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    
-                    Text("How it's your day going?")
-                        .font(.system(size: 25, weight: .medium, design: .monospaced))
-                        .foregroundStyle(.white)
-                        
-                }
-                Spacer()
-            }
-            
+        VStack{
+            Image("cabecadosharl")
+            Text("Hello World")
+                .font(.system(size: 29, weight: .medium, design: .monospaced))
+            Circle()
+                .stroke(lineWidth: 10)
+                .frame(width: 100, height: 100, alignment: .top)
+            Rectangle()
+                .stroke(lineWidth: 20)
+                .frame(width: 100, height: 100, alignment: .bottom)
+                .cornerRadius(20)
+                
+            Capsule()
+                .fill(.cyan)
+                .frame(width: 100, height: 50)
         }
+        
+//        ZStack{
+//            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+//                           startPoint: .top,
+//                           endPoint: .bottom)
+//            .ignoresSafeArea(.all)
+//
+//            VStack{
+//                Text("Belo Horizonte, MG")
+//                    .font(.system(size: 32, weight: .medium, design: .default))
+//                    .foregroundStyle(.white)
+//                    .padding()
+//                VStack{
+//                    Image(systemName: "cloud.sun.fill")
+//                        .renderingMode(.original)
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fit)
+//                        .frame(width: 180, height: 180)
+//
+//                    Text("How it's your day going?")
+//                        .font(.system(size: 25, weight: .medium, design: .monospaced))
+//                        .foregroundStyle(.white)
+//
+//                }
+//                Spacer()
+//            }
+//
+//        }
         
     }
 }
