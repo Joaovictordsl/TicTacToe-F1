57ac62cf95617e42280e099a1a9d53b0e160f909
Joaovictordsl <joaovictordsl00@gmail.com>
2024-01-19 00:38:56 -0300

    trying stuff again

--- TicTacToe F1/ContentView.swift
+++ TicTacToe F1/ContentView.swift
@@ -9,22 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [Color.black, Color.green]), startPoint: .top, endPoint: .bottom)
+        
+        ZStack {
+            LinearGradient(colors: [.red, .black, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+            
+            VStack (alignment: .leading) {
                 
-            VStack{
-                Image(systemName: "heart.fill")
-                    .renderingMode(.template)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 50, height: 50)
-                    .foregroundColor(.green)
+                HStack {
+                    Image("cabecadosharl")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 100, height: 100, alignment: .leading)
                     
+                    VStack (alignment: .leading) {
+                        Text("Sharl")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("@charles_leclerc")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+
+                    }
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image("cabecadovespa")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 100, height: 100, alignment: .leading)
+                    
+                    VStack (alignment: .leading) {
+                        Text("Vespa")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("@verstappen")
+                            .foregroundColor(.white)
+                            .font(.subheadline)
+                    }
+                    
+                    
+                }
             }
         }
+        
+        
+        
+        
+    }
         
         
         
@@ -56,7 +97,7 @@ struct ContentView: View {
 //
 //        }
         
-    }
+    
 }
 
 #Preview {
