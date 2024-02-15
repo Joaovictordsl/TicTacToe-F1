bee4b7274640fdc739efbe2b22c689188b244640
Joaovictordsl <joaovictordsl00@gmail.com>
2024-02-14 23:57:03 -0300

    h

---
+++ TicTacToe F1/TicTacToeView.swift
@@ -0,0 +1,18 @@
+//
+//  TicTacToeView.swift
+//  TicTacToe F1
+//
+//  Created by Pedro on 09/02/24.
+//
+
+import SwiftUI
+
+struct TicTacToeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TicTacToeView()
+}
